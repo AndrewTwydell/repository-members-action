7ff6a466a2e3415edcfa81d5e639e64f70c2a067
Andrew Twydell <andrew.twydell@ibm.com>
2023-02-01 10:26:18 +0000

    rest api call
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
 
-try {
-    const ghToken = core.getInput('token');
-    console.log(ghToken);
-    core.setOutput("members", ["andrew", "Tom"]);
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-} catch (error) {
-    core.setFailed(error.message);
-}
+const getMembers = async () => {
+    try {
+        const ghToken = core.getInput('token');
+        const owner = core.getInput('owner');
+        const repo = core.getInput('repo');
+
+        const newOctokit = new Octokit({
+            auth: ghToken
+        });
+        const stuff = await newOctokit.rest.repos.getUsersWithAccessToProtectedBranch({
+            owner,
+            repo,
+            branch: "main",
+        });
+        core.setOutput("members", stuff);
+        console.log(stuff);
+        // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
+
+getMembers();
